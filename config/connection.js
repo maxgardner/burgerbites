ac446f47d033026fa8b927671553d19e9223b21c
Max Gardner <max.c.gardner@gmail.com>
2017-04-30 14:49:15 -0500

    Add config files for database

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 let mysql = require("mysql");
 let connection;
 
-if (process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {""
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
